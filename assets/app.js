633c06c6a09d0e7ab21ceca64de80f8e7f57a257
Reggie P <reginaldpitts1@gmail.com>
2022-11-25 15:58:37 -0600

    add event listeners to app js file

---
+++ assets/app.js
@@ -0,0 +1,11 @@
+// this file will call the methods from other files
+
+const fetch6 = new Fetch();
+const ui6 = new UI();
+
+// add event listener(s) here
+const button = document.getElementById("submit"); // line 37 of html
+
+const search = document.getElementById("searchUser"); // line 28 of html
+
+
